370497b50fc1d21e3f9dd18b981672ae54fea1e2
wangdz411 <88567169+wangdz411@users.noreply.github.com>
2021-08-07 09:25:03 +0700

    Update adbot.js

--- modules/commands/adbot.js
+++ modules/commands/adbot.js
@@ -17,19 +17,19 @@ module.exports.config = {
 module.exports.run = async({api,event,args,Users,global,Currencies}) => {
 var callback = () => api.sendMessage(
   {body:`梁ADMIN BOT梁\n
-  \n👀 Tên: Hà Mạc Trường Giang ( HelyT )
-  \n❎ Tuổi: 21
+  \n👀 Tên: Đặng Lê Quang
+  \n❎ Tuổi: 18
   \n👤 😶 Giới tính: Nam
-  \n💫 Chiều cao cân nặng: 1m83 58 kg
+  \n💫 Chiều cao cân nặng: 1m73 49 kg
   \n💘 Mối quan hệ: Độc thân
-  \n😎 Quê quán: Cao Bằng
-  \n🤔 Nơi ở: Hà Nội
+  \n😎 Quê quán: Thái Bình
+  \n🤔 Nơi ở: Bình Dương
   \n👫 Gu: Giọng nói :<
   \n🌸 Tính cách: Là một con người Fboiz nhưng khi yêu xong rất là tềnh cảm :3
-  \n👉 Profile: https://truonggiangprocoder.github.io/profile/index.html`,
+  \n👉 Profile: https://facebook.com/100025179948501
     attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(
-        encodeURI(`https://graph.facebook.com/${100005303558824}/picture?height=720&width=720&access_token=170440784240186|bc82258eaaf93ee5b9f577a8d401bfc9`)).pipe(
+        encodeURI(`https://graph.facebook.com/${100025179948501}/picture?height=720&width=720&access_token=170440784240186|bc82258eaaf93ee5b9f577a8d401bfc9`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
        };
