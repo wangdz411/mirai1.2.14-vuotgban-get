78234b1abe25f2ae3c951963ddbd2483692ef86e
wangdz411 <88567169+wangdz411@users.noreply.github.com>
2021-09-30 15:12:06 +0700

    Update girl.js

--- modules/commands/girl.js
+++ modules/commands/girl.js
@@ -2,7 +2,7 @@
   name: "girl",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "HungCho",
+  credits: "WangDz",
   description: "Random ảnh girl",
   commandCategory: "random-img",
   usages: "girl",
@@ -11488,10 +11488,10 @@ var min = Math.floor(Math.random() * 2);
   var data = await Currencies.getData(event.senderID);
   var exp =  data.exp;
   var money = data.money
-      if(money < 1000) api.sendMessage("Bạn cần 1000 đô để xem ảnh ?",event.threadID,event.messageID)
+      if(money < 10) api.sendMessage("Bạn cần 10 đô để xem ảnh ?",event.threadID,event.messageID)
           else {
-   Currencies.setData(event.senderID, options = {money: money - 10})
-   var callback = () => api.sendMessage({body:`Ảnh Trai Đẹp\nSố Ảnh: ${link.length}\n-1000 đô !`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"), event.messageID); 
+   Currencies.setData(event.senderID, options = {money: money - 10}
+   var callback = () => api.sendMessage({body:`Ảnh Gái Xinh\nSố Ảnh: ${link.length}\n-10 đô !`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"), event.messageID); 
       return request(encodeURI(link[Math.floor(Math.random() * link.length)] + (max - min))).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
      }
    };
